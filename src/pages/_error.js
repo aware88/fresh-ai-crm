b5c0bb54d611335ec61b2de0fc640bcf850ca6b9
Your GitHub Username <your.github@email.com>
2025-06-13 11:19:13 +0200

    Add custom error pages to handle prerender issues

---
+++ src/pages/_error.js
@@ -0,0 +1,19 @@
+function Error({ statusCode }) {
+  return (
+    <div style={{ padding: '20px', textAlign: 'center' }}>
+      <h1>
+        {statusCode
+          ? `An error ${statusCode} occurred on server`
+          : 'An error occurred on client'}
+      </h1>
+      <p>Please try again later.</p>
+    </div>
+  )
+}
+
+Error.getInitialProps = ({ res, err }) => {
+  const statusCode = res ? res.statusCode : err ? err.statusCode : 404
+  return { statusCode }
+}
+
+export default Error
